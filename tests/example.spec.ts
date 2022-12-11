293c2f4e0e3b6c8412f9ca25ba9012f28afcbfad
kohei <bhkbb0551@gmail.com>
2022-12-11 18:02:43 +0900

    [feat] add example test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,15 +1,84 @@
 import { test, expect } from '@playwright/test'
 
-test('Simple Basic test', async ({ page }) => {
+test('ベーシック', async ({ page }) => {
   await page.goto('https://www.example.com')
   const pageTitle = await page.locator('h1')
   await expect(pageTitle).toContainText('Example Domain')
 })
 
-test('Clickin on Elements', async({page}) =>{
+test('クリック', async ({ page }) => {
   await page.goto('http://zero.webappsecurity.com/index.html')
   await page.click('#signin_button')
   await page.click('text=Sign in')
 
-  await expect(page.locator('.alert-error')).toHaveText('Login and/or password are wrong.')
+  await expect(page.locator('.alert-error')).toHaveText(
+    'Login and/or password are wrong.'
+  )
+})
+
+// .onlyを使うと、これだけを実行することになる。
+test.only('入力', async ({ page }) => {
+  await page.goto('http://zero.webappsecurity.com/index.html')
+  await page.click('#signin_button')
+
+  await page.type('#user_login', 'some username')
+  await page.locator('#user_password').type('some password')
+
+  await page.click('text=Sign in')
+
+  await expect(page.locator('.alert-error')).toContainText(
+    'Login and/or password are wrong.'
+  )
+})
+
+// .onlyを複数つけると、onlyをつけたものだけ実行するようになる。
+test.only('アサーション', async ({ page }) => {
+  await page.goto('https://example.com')
+  await expect(page).toHaveURL('https://example.com')
+  await expect(page).toHaveTitle('Example Domain')
+
+  const exsistElement = page.locator('h1')
+  await expect(exsistElement).toBeVisible()
+
+  await expect(exsistElement).toHaveText('Example Domain')
+  //locatorが存在する数を検証。 選択肢がある場合などは使えるかも。
+  await expect(exsistElement).toHaveCount(1)
+
+  const nonexsistElement = page.locator('h5')
+  await expect(nonexsistElement).not.toBeVisible()
+})
+
+// describeの前にonly,skipなどは使えない。
+test.describe('describeを使ってテストをまとめることができる', () => {
+  // .onlyを使うと、これだけを実行することになる。
+  test.only('入力', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+
+    await page.type('#user_login', 'some username')
+    await page.locator('#user_password').type('some password')
+
+    await page.click('text=Sign in')
+
+    await expect(page.locator('.alert-error')).toContainText(
+      'Login and/or password are wrong.'
+    )
+  })
+
+  // .onlyを複数つけると、onlyをつけたものだけ実行するようになる。
+  test.only('アサーション', async ({ page }) => {
+    await page.goto('https://example.com')
+    await expect(page).toHaveURL('https://example.com')
+    await expect(page).toHaveTitle('Example Domain')
+
+    const exsistElement = page.locator('h1')
+    await expect(exsistElement).toBeVisible()
+
+    await expect(exsistElement).toHaveText('Example Domain')
+    //locatorが存在する数を検証。 選択肢がある場合などは使えるかも。
+    await expect(exsistElement).toHaveCount(1)
+
+    const nonexsistElement = page.locator('h5')
+    await expect(nonexsistElement).not.toBeVisible()
+  })
 })
