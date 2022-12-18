8137eff29b4c516f5c427a5923a78dbcc2f6ab2c
kohei <bhkbb0551@gmail.com>
2022-12-18 16:44:53 +0900

    セレクターの > と >> の違いを記載

--- testsE2E/search.spec.ts
+++ testsE2E/search.spec.ts
@@ -8,6 +8,16 @@ test.describe('検索結果', () => {
     await page.type('#searchTerm','bank')
     await page.keyboard.press('Enter')
 
+    /**
+     *   selector の　「>」 と　「>>」の違い
+     * 
+     *   'XXX > YYY' は、CSSセレクターを階層的に取得する。
+     *   'XXX >> YYY' は、CSSセレクターと、テキストセレクターなどを組み合わせて階層的に取得することができる。
+     * 
+     *   ※　li > text='hello' は無理だが、
+     *      li >> text='hello' は可能。
+     */
+
     const NumberOfLinks = await page.locator('li > a')
     await expect(NumberOfLinks).toHaveCount(2)
   })
