70be740fc23302d8b4ff7af35eb541ee596e1642
kohei <bhkbb0551@gmail.com>
2022-12-25 15:53:09 +0900

    [feat] ログインページのテスト

--- testsE2E/login.spec.ts
+++ testsE2E/login.spec.ts
@@ -1,36 +1,32 @@
 import { test, expect } from '@playwright/test'
+import { LoginPage } from '../page-objects/LoginPage'
+import { HomePage } from '../page-objects/HomePage'
 
 test.describe('Login / Logout Flow', () => {
-  // Before Hook
+  let loginPage: LoginPage
+  let homePage: HomePage
 
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/')
+    loginPage = new LoginPage(page)
+    homePage = new HomePage(page)
+    await homePage.visit()
   })
 
-  // Negative Senario
   test('Negative Scenario', async ({ page }) => {
-    await page.click('#signin_button')
-    await page.type('#user_login', 'invalid username')
-    await page.type('#user_password', 'invalid password')
-    await page.click('text=Sign in')
-
-    await expect(page.locator('.alert-error')).toContainText(
-      'Login and/or password are wrong.'
-    )
+    // await page.click('#signin_button')
+    await homePage.clickOnSignIn()
+    await loginPage.login('invalid username', 'invalid password')
+    await loginPage.assertErrorMessage()
   })
 
-  // Positive Scenario
   test('Positive Scenario for login + logout', async ({ page }) => {
-    await page.click('#signin_button')
-    await page.type('#user_login', 'username')
-    await page.type('#user_password', 'password')
-    await page.click('text=Sign in')
+    await homePage.clickOnSignIn()
+    await loginPage.login('username', 'password')
 
     await page.goto('http://zero.webappsecurity.com/bank/account-summary.html')
-    
+
     await expect(page.locator('#account_summary_tab')).toBeVisible()
-    
-    page.pause()
+
     await page.goto('http://zero.webappsecurity.com/logout.html')
     await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
   })
